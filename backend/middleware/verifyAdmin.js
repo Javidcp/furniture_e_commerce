7fd73c55db649b3096bac688f9effa36aa0b7355
javid ibrahim <ibrupottayil@gmail.com>
2025-05-10 16:01:42 +0530

    10 05

--- backend/middleware/verifyAdmin.js
+++
@@ -1,8 +0,0 @@
-export const verifyAdmin = (req, res, next) => {
-    if (req.user && req.user.role === "admin") {
-      next();
-    } else {
-      return res.status(403).json({ message: 'Access denied, Admins only!' });
-    }
-  };
-    
--- backend/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createCheckoutSession } from '../controllers/paymentController.js';
-import { verifyToken } from '../middleware/verifyToken.js';
-
-const router = express.Router();
-
-router.post('/create-checkout-session',verifyToken, createCheckoutSession);
-
-export default router;
